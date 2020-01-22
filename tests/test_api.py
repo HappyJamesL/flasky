22ebf86e18d7d306cb22c2d9af7acb17d7008d9d
JamesL <HappyJamesL@users.noreply.github.com>
2020-01-22 17:20:24 +0800

    test

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,7 @@ import re
 from base64 import b64encode
 from app import create_app, db
 from app.models import User, Role, Post, Comment
-
+import os
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
